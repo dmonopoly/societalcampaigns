fa1f71898f4f211fe373d47fc6dea97f4b4ccc5f
David Zhang <dzhang21@gmail.com>
2011-10-01 14:11:10 -0700

    First commit, with campaign scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Societalcampaigns::Application.config.secret_token = '9bb6a3d6da66e8b9a3005182dafa06cd7ae402e8a61846f2575789df9217507a63c074d4026352dcf9f1f856b5450675a9147bc07b554a8e2e9e2502e6a14340'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Societalcampaigns::Application.routes.draw do
+  resources :campaigns
+
+  root :to => 'campaigns#index'
+end
